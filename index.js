67a92c21f00ab8c54d4bd87c11d7a1bea595dc33
Tenebrosful <hugobernard39@gmail.com>
2021-06-14 03:11:47 +0200

    add command and event loader

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
+const fs = require('fs');
 const Discord = require('discord.js');
-const client = new Discord.Client();
+require('discord-reply');
 const config = require('./config.json');
 
-client.once('ready', () => {
-    console.log('ready !');
-});
+const client = new Discord.Client();
 
+loadCommands();
+loadEvents();
 client.login(config.bot_token);
+
+function loadEvents() {
+    const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+
+    for (const file of eventFiles) {
+        const event = require(`./events/${file}`);
+        if (event.once) {
+            client.once(event.name, (...args) => event.execute(...args, client, config));
+        } else {
+            client.on(event.name, (...args) => event.execute(...args, client, config));
+        }
+    }
+}
+
+function loadCommands() {
+    config.commands = new Discord.Collection();
+
+    const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+    for (const file of commandFiles) {
+        const command = require(`./commands/${file}`);
+        config.commands.set(command.name, command);
+    }
+}
