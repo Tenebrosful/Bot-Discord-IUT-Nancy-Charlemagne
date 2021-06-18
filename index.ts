00cf267f48e5068e0a3aef733ba5bf9473b35cc2
Tenebrosful <hugobernard39@gmail.com>
2021-06-18 05:32:14 +0200

    Add IDs enums

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import 'reflect-metadata';
 import { Intents } from 'discord.js';
 import { Client } from '@typeit/discord';
 import * as config from './configs/bot.json';
+import { Server } from './enums/IDs';
 
 async function start() {
     const client = new Client({
@@ -11,12 +12,12 @@ async function start() {
         classes: [
             `${__dirname}/commands/*.ts`
         ],
-        slashGuilds: ["753171392450527282"]
+        slashGuilds: [Server.MAIN]
     });
 
     client.once("ready", async () => {
         await client.clearSlashes();
-        await client.clearSlashes('753171392450527282');
+        await client.clearSlashes(Server.MAIN);
         await client.initSlashes();
     });
 
