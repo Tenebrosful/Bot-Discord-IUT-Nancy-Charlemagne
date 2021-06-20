6b47b770065840fb2ae4003f89e0c1582a568693
Tenebrosful <hugobernard39@gmail.com>
2021-06-20 04:07:52 +0200

    Handle buttons and log interactions

--- index.ts
+++ index.ts
@@ -1,8 +1,7 @@
 import 'reflect-metadata';
-import { Intents } from 'discord.js';
+import { ButtonInteraction, CommandInteraction, DiscordAPIError, Intents, Interaction, MessageComponentInteraction, TextChannel } from 'discord.js';
 import { Client } from '@typeit/discord';
 import * as config from './configs/bot.json';
-import { Server } from './enums/IDs';
 
 export let SingletonClient: Client;
 
@@ -22,12 +21,47 @@ async function start() {
         console.log('Ready !');
     });
 
-        try { client.executeSlash(interaction); } catch(error) { console.error(error) }
-        
     SingletonClient.on("interaction", (interaction) => {
+        console.log(logInteraction(interaction));
+
+        switch (interaction.type) {
+            case 'APPLICATION_COMMAND':
+                try { SingletonClient.executeSlash(interaction); } catch (error) { console.error(error) }
+                break;
+            case 'MESSAGE_COMPONENT':
+                if (interaction.isButton) {
+                    (<ButtonInteraction>interaction).reply({ content: "Peut être un jour, il y aura une fonction qui pourrait par exemple... remplacer les réactions pour obtenir un rôle en beaucoup plus propre, mais pour l'instant il n'y a rien.", ephemeral: true });
+                } else {
+                    (<MessageComponentInteraction>interaction).reply({ content: "Cette fonctionnalité n'a pas encore été implémenté, désolé :c", ephemeral: true });
+                }
+
+                break;
+        }
     });
 
     await SingletonClient.login(config.bot_token);
+}
+
+export function getHorodateConsole() {
+    const date: Date = new Date(Date.now());
+
+    return `[${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}]`
+}
+
+function logInteraction(interaction: Interaction) {
+    let log = `${getHorodateConsole()}\t${interaction.user.username}\t${interaction.type}`
+
+    switch (interaction.type) {
+        case 'APPLICATION_COMMAND':
+            log += `\t${(<CommandInteraction>interaction).commandName}`;
+            break;
+        case 'MESSAGE_COMPONENT':
+            break;
+        case 'PING':
+            break;
+    }
+
+    return log;
 }
 
 start();
