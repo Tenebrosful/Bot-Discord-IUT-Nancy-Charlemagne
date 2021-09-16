1d34791285d82f1b84eee25fc7d94d4fb4373297
Tenebrosful <hugobernard39@gmail.com>
2021-09-16 13:50:22 +0200

    Create GlobalVar.ts

---
+++ global/GlobalVar.ts
@@ -0,0 +1,17 @@
+import { ApplicationCommandPermissionData } from "discord.js";
+import { Serveur } from "../IDs";
+import { Role as Role_IUT_NC_DEPINFO } from "../iut_nc_depinfo/IDs";
+
+export const globalAdminPerms: ApplicationCommandPermissionData[] = [
+    { id: Role_IUT_NC_DEPINFO.STAR, type: "ROLE", permission: true },
+    { id: Role_IUT_NC_DEPINFO.ADMIN, type: "ROLE", permission: true }
+]
+
+export const globalEnseignantPerms: ApplicationCommandPermissionData[] = [
+    { id: Role_IUT_NC_DEPINFO.ENSEIGNANT, type: "ROLE", permission: true }
+]
+
+export const allServeursIds: Serveur[] = [
+    Serveur.IUT_NC_DEP_INFO,
+    Serveur.IUT_NC_DEP_INFO2
+]
