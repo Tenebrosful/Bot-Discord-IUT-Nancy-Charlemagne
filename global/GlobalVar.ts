b2ffc06bb19b811c0553187eb73605be8ec8516a
Tenebrosful <hugobernard39@gmail.com>
2021-09-17 02:24:17 +0200

    Ajout des perms et serveurs bêta

--- global/GlobalVar.ts
+++ global/GlobalVar.ts
@@ -4,7 +4,9 @@ import { Role as Role_IUT_NC_DEPINFO } from "../iut_nc_depinfo/IDs";
 
 export const globalAdminPerms: ApplicationCommandPermissionData[] = [
     { id: Role_IUT_NC_DEPINFO.STAR, type: "ROLE", permission: true },
-    { id: Role_IUT_NC_DEPINFO.ADMIN, type: "ROLE", permission: true }
+    { id: Role_IUT_NC_DEPINFO.ADMIN, type: "ROLE", permission: true },
+    //{ id: RoleBeta.BETA_1, type: "ROLE", permission: true },
+    //{ id: RoleBeta.BETA_2, type: "ROLE", permission: true }
 ]
 
 export const globalEnseignantPerms: ApplicationCommandPermissionData[] = [
@@ -13,5 +15,7 @@ export const globalEnseignantPerms: ApplicationCommandPermissionData[] = [
 
 export const allServeursIds: Serveur[] = [
     Serveur.IUT_NC_DEP_INFO,
-    Serveur.IUT_NC_DEP_INFO2
+    //Serveur.BETA_2,
+    //Serveur.BETA_1
+]
 ]
